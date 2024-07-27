03bf7df3f3439622a3403fec31c91a228bd77da5
kamkom84 <kbd.dimitrov@gmail.com>
2024-07-27 12:31:13 +0300

    commit: patch

--- src/test/java/tests/PATCH.java
+++ src/test/java/tests/PATCH.java
@@ -1,4 +1,33 @@
 package tests;
 
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class PATCH {
+
+    @Test(priority = 2)
+    public void put() {
+
+        JSONObject request = new JSONObject();
+
+        request.put("name", "Popeye");
+        request.put("job", "sailor");
+
+        System.out.println(request.toJSONString());
+
+        given().
+                header("Content-Type", "application/json").
+                contentType(ContentType.JSON).accept(ContentType.JSON).
+                body(request.toJSONString()).
+                when().
+                patch("https://reqres.in/api/users/2").
+                then().
+                statusCode(200).
+                log().all();
+
+    }
+
 }
