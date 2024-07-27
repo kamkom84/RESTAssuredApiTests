7d121cb268d4979f43597573bcbed1677993be3f
kamkom84 <kbd.dimitrov@gmail.com>
2024-07-27 12:23:58 +0300

    commit

---
+++ src/test/java/tests/POST.java
@@ -0,0 +1,33 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+
+public class POST {
+
+    @Test(priority = 1)
+    public void post() {
+
+        JSONObject request = new JSONObject();
+
+        request.put("name", "John");
+        request.put("job", "sailor");
+
+        System.out.println(request.toJSONString());
+
+        given().
+                header("Content-Type", "application/json").
+                contentType(ContentType.JSON).accept(ContentType.JSON).
+                body(request.toJSONString()).
+                when().
+                post("https://reqres.in/api/users").
+                then().
+                statusCode(201);
+
+    }
+
+}
---
+++ src/test/java/tests/PUT.java
@@ -0,0 +1,33 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PUT {
+
+    @Test(priority = 2)
+    public void put() {
+
+        JSONObject request = new JSONObject();
+
+        request.put("name", "Popeye");
+        request.put("job", "sailor");
+
+        System.out.println(request.toJSONString());
+
+        given().
+                header("Content-Type", "application/json").
+                contentType(ContentType.JSON).accept(ContentType.JSON).
+                body(request.toJSONString()).
+                when().
+                put("https://reqres.in/api/users/2").
+                then().
+                statusCode(200).
+                log().all();
+
+    }
+
+}
